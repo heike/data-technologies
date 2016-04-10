3044572bc4b8762019f6eab1b87e0537f6af9ca7
Dianne Cook <visnut@gmail.com>
2016-04-11 07:30:15 +1000

    getting the map stuff organised

--- r_use.R
+++ r_use.R
@@ -4,44 +4,55 @@ library("dplyr")
 library("rworldmap")
 library("purrr")
 
-rsurvey <- read_delim("data/pnas.1506047112.sd01.csv", delim=";")
+#rsurvey <- read_delim("data/pnas.1506047112.sd01.csv", delim=";")
 
-head(rsurvey$v_253)
-table(rsurvey$v_253)
+#head(rsurvey$v_253)
+#table(rsurvey$v_253)
 
 datraw <- read.csv2("data/pnas.1506047112.sd01.csv", dec = ".")
 table(datraw$v_253)
-datraw$v_253[datraw$v_253 == 204] <- NA
+#datraw$v_253[datraw$v_253 == 204] <- NA
 
+# Match names to country code
 countrynames <- read.table("data/pnas.1506047112.country.txt", stringsAsFactors = FALSE)
 colnames(countrynames) <- c("indx", "name")
+countrynames <- rbind(countrynames, c(204, "unknown"))
 
 datraw$country <- countrynames$name[datraw$v_253]
+datraw$country[is.na(datraw$country)] <- "unknown"
+table(datraw$country)
 
 world <- getMap(resolution = "low")
-
-length(world@polygons)
-
-world %>% group_by()
-
-# Need to modify this function
-extractPolygons <- function(shapes) {
-  
-  dframe <- ldply(1:length(shapes@polygons), function(i) {
-    ob <- shapes@polygons[[i]]@Polygons
-    dframe <- ldply(1:length(ob), function(j) {
-      x <- ob[[j]]
-      co <- x@coords
-      data.frame(co, order=1:nrow(co), group=j)
-    })
-    dframe$region <- i
-    dframe$name <- shapes@polygons[[i]]@ID
-    dframe
-  })
-  # construct a group variable from both group and polygon:
-  dframe$group <- interaction(dframe$region, dframe$group)
-  
-  dframe
+extractPolys <- function(p) {
+  polys <- NULL
+  for (i in 1:length(p)) {
+    for (j in 1:length(p[[i]]@Polygons)) {
+      x <- p[[i]]@Polygons[[j]]@coords
+      polys$lon <- c(polys$lon, x[,1])
+      polys$lat <- c(polys$lat, x[,2])
+      polys$ID <- c(polys$ID, rep(p[[i]]@ID, nrow(x)))
+      polys$region <- c(polys$region, rep(paste(p[[i]]@ID, j, sep="_"), nrow(x)))
+      polys$order <- c(polys$order, 1:nrow(x))
+    }
+  }
+  return(data.frame(polys))
 }
+polys <- extractPolys(world@polygons)
+  
+qplot(lon, lat, data=polys, group=region, geom="path")
+qplot(lon, lat, data=polys, group=region, geom="polygon")
+
+# Match country names to map names
+cntrynames <- unique(datraw$country)
+polynames <- unique(polys$ID)
+setdiff(cntrynames, polynames)
+
+# Tabulate the countributing countries
+cntry_count <- datraw %>% group_by(country) %>% tally()
+
+# Join to map
+polys_cntry <- merge(polys, cntry_count, by.x="ID", by.y="country", all.x=TRUE)
+polys_cntry <- polys_cntry %>% arrange(region, order)
+qplot(lon, lat, data=polys_cntry, group=region, geom="polygon", fill=n) + coord_equal()
 
 
